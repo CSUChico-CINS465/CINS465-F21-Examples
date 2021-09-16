caec585aa44aac99a1fba16162be991f5c439f97
Bryan Dixon <javawolfpack@gmail.com>
2021-09-16 10:44:27 -0700

    Basic Models

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import random
+
+from . import models
 
 # Create your views here.
-def index(request, page=0):
-    some_list = list(range(page*10,page*10+10,1))
+def index(request):
+    some_list = models.SuggestionModel.objects.all()
     context = {
         "title": "CINS465",
         "body":"Hello World",
         "some_list": some_list,
-        "next":page+1,
-        "prev":page-1
     }
     return render(request,"index.html", context=context)
+
+def dumb(request):
+    some_list = models.SuggestionModel.objects.all()
+    some_int = random.randrange(len(some_list))
+    some_instance = some_list[some_int]
+    some_instance.delete()
+    print(some_int)
+    return redirect("/")
